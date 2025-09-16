dd85edf26877718e974cc5e684e879265f771808
Will Hedgecock <will.hedgecock@gmail.com>
2025-09-16 15:25:03 -0500

    Temporarily disable PyTorch parsing until ai-edge-lite bug is fixed

--- microesc/tools/compat.py
+++ microesc/tools/compat.py
@@ -1,12 +1,12 @@
-import torch
+#import torch
 import numpy as np
-import ai_edge_torch
+#import ai_edge_torch
 import tensorflow as tf
 import tensorflow_model_optimization as tfmot
-from ai_edge_torch.quantize import pt2e_quantizer, quant_config
+#from ai_edge_torch.quantize import pt2e_quantizer, quant_config
 from ai_edge_litert.interpreter import Interpreter
-from torch.ao.quantization import quantize_pt2e
-from typing import Literal
+#from torch.ao.quantization import quantize_pt2e
+#from typing import Literal
 from microesc import keras
 from .. import PyDataset
 
@@ -26,24 +26,24 @@ def convert_tensorflow_to_tflite(saved_model_dir: str, output_file: str, quantiz
   with open(output_file, 'wb') as f:
       f.write(tflite_model)  # type: ignore
 
-def convert_pytorch_to_tflite(model: torch.nn.Module, test_dataset: PyDataset, output_file: str, quantize: Literal['p2te', 'tf'] | None, input_shape: tuple | None = None):
-  input_shape = input_shape if input_shape else next(model.parameters()).size()
-  quant_cfg, quant_flags = None, None
-  if quantize == 'pt2e':
-    quantizer = pt2e_quantizer.PT2EQuantizer().set_global(pt2e_quantizer.get_symmetric_quantization_config(is_per_channel=True))
-    model = torch.export.export_for_training(model.eval(), (torch.randn(input_shape),)).module()
-    model = quantize_pt2e.prepare_pt2e(model, quantizer)  # type: ignore
-    for i in range(len(test_dataset)):
-      for data in test_dataset[i][0]:
-        model(data)
-    model = quantize_pt2e.convert_pt2e(model, fold_quantize=False)
-    quant_cfg = quant_config.QuantConfig(pt2e_quantizer=quantizer)
-  elif quantize == 'tf':
-    quant_flags = {'optimizations': [tf.lite.Optimize.DEFAULT]}
-  else:
-    model = model.eval()
-  tflite_model = ai_edge_torch.convert(model, (torch.randn(input_shape),), quant_config=quant_cfg, _ai_edge_converter_flags=quant_flags)
-  tflite_model.export(output_file)
+#def convert_pytorch_to_tflite(model: torch.nn.Module, test_dataset: PyDataset, output_file: str, quantize: Literal['p2te', 'tf'] | None, input_shape: tuple | None = None):
+#  input_shape = input_shape if input_shape else next(model.parameters()).size()
+#  quant_cfg, quant_flags = None, None
+#  if quantize == 'pt2e':
+#    quantizer = pt2e_quantizer.PT2EQuantizer().set_global(pt2e_quantizer.get_symmetric_quantization_config(is_per_channel=True))
+#    model = torch.export.export_for_training(model.eval(), (torch.randn(input_shape),)).module()
+#    model = quantize_pt2e.prepare_pt2e(model, quantizer)  # type: ignore
+#    for i in range(len(test_dataset)):
+#      for data in test_dataset[i][0]:
+#        model(data)
+#    model = quantize_pt2e.convert_pt2e(model, fold_quantize=False)
+#    quant_cfg = quant_config.QuantConfig(pt2e_quantizer=quantizer)
+#  elif quantize == 'tf':
+#    quant_flags = {'optimizations': [tf.lite.Optimize.DEFAULT]}
+#  else:
+#    model = model.eval()
+#  tflite_model = ai_edge_torch.convert(model, (torch.randn(input_shape),), quant_config=quant_cfg, _ai_edge_converter_flags=quant_flags)
+#  tflite_model.export(output_file)
 
 def load_keras_model(model_path: str) -> keras.Model:
   with tfmot.quantization.keras.quantize_scope():
